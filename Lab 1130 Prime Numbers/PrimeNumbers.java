3b228dcb4a0cc4fe4132f608b44977f1c3af613e
795965 <42558488+795965@users.noreply.github.com>
2018-12-03 21:05:23 -0800

    started Prime Numbers

---
+++ Lab 1130 Prime Numbers/PrimeNumbers.java
@@ -0,0 +1,22 @@
+
+/**
+ * Function to find largest Prime Number
+ *
+ * @author (Ziggy Sheynin)
+ * @version (Lab 1130)
+ */
+import java.util.ArrayList;
+public class PrimeNumbers
+{
+    //takes an integer as an input and puts each of the digits into 
+    //an ArrayList of Integer objects.  
+    public  void setListToNumbers (int n){
+    }
+    //takes an integer and adds it to an existing ArrayList of Integer objects
+    public void addNumberToList(int n){
+    }
+    //takes an integer and adds it to an existing ArrayList of Integer 
+    //objects
+    public void addNumbers(ArrayList<Integer> n1, ArrayList<Integer> n2){
+    }
+}
