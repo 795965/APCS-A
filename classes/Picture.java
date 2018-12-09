23af24bc6935099ca92464e668363839075cbf13
795965 <42558488+795965@users.noreply.github.com>
2018-12-09 11:35:56 -0800

    Finished Project Picture Lab + Color Edge Detection

--- classes/Picture.java
+++ classes/Picture.java
@@ -97,19 +97,6 @@ public class Picture extends SimplePicture
             }
         }
     }
-    /** Method to keep only Blue */
-    public void OnlyBlue()
-    {
-        Pixel[][] pixels = this.getPixels2D();
-        for (Pixel[] rowArray : pixels)
-        {
-            for (Pixel pixelObj : rowArray)
-            {
-                pixelObj.setRed(0);
-                pixelObj.setGreen(0);
-            }
-        }
-    }
 
     /** Method that mirrors the picture around a 
      * vertical mirror in the center of the picture
@@ -131,47 +118,6 @@ public class Picture extends SimplePicture
         } 
     }
 
-    //method that mirrors picture around mirror vertically from right to left
-    /** Method that mirrors the picture around a 
-     * vertical mirror in the center of the picture
-     * from left to right */
-    public void mirrorVerticalRightToLeft()
-    {
-        Pixel[][] pixels = this.getPixels2D();
-        Pixel leftPixel = null;
-        Pixel rightPixel = null;
-        int width = pixels[0].length;
-        for (int row = 0; row < pixels.length; row++)
-        {
-            for (int col = 0; col < width / 2; col++)
-            {
-                leftPixel = pixels[row][col];
-                rightPixel = pixels[row][width - 1 - col];
-                leftPixel.setColor(rightPixel.getColor());
-            }
-        } 
-    }
-    //mirrors viretically top to bottom
-    /** Method that mirrors the picture around a 
-     * vertical mirror in the center of the picture
-     * from left to right */
-        // public void mirrorHorizontal()
-        // {
-            // Pixel[][] pixels = this.getPixels2D();
-            // Pixel topPixel = null;
-            // Pixel bottomPixel = null;
-            // int height = pixels[].length;
-            // for (int row = 0; row < pixels.length; row++)
-            // {
-                // for (int col = 0; col < height / 2; col++)
-                // {
-                    // topPixel = pixels[row][col];
-                   // bottomPixel = pixels[row][height - 1 - col];
-                    // bottomPixel.setColor(topPixel.getColor());
-                // }
-            // } 
-        // }
-    //Activity 7
     /** Mirror just part of a picture of a temple */
     public void mirrorTemple()
     {
@@ -195,7 +141,7 @@ public class Picture extends SimplePicture
             }
         }
     }
-    //Activity 8
+
     /** copy from the passed fromPic to the
      * specified startRow and startCol in the
      * current picture
@@ -210,12 +156,12 @@ public class Picture extends SimplePicture
         Pixel toPixel = null;
         Pixel[][] toPixels = this.getPixels2D();
         Pixel[][] fromPixels = fromPic.getPixels2D();
-        for (int fromRow = 250, toRow = startRow; 
+        for (int fromRow = 0, toRow = startRow; 
         fromRow < fromPixels.length &&
         toRow < toPixels.length; 
         fromRow++, toRow++)
         {
-            for (int fromCol = 250, toCol = startCol; 
+            for (int fromCol = 0, toCol = startCol; 
             fromCol < fromPixels[0].length &&
             toCol < toPixels[0].length;  
             fromCol++, toCol++)
@@ -226,16 +172,15 @@ public class Picture extends SimplePicture
             }
         }   
     }
-    
-    //Activity 8
+
     /** Method to create a collage of several pictures */
     public void createCollage()
     {
         Picture flower1 = new Picture("flower1.jpg");
         Picture flower2 = new Picture("flower2.jpg");
-        this.copy(flower1,0,50);
-        this.copy(flower2,100,100);
-        this.copy(flower1,300,0);
+        this.copy(flower1,0,0);
+        this.copy(flower2,100,0);
+        this.copy(flower1,200,0);
         Picture flowerNoBlue = new Picture(flower2);
         flowerNoBlue.zeroBlue();
         this.copy(flowerNoBlue,300,0);
@@ -249,19 +194,16 @@ public class Picture extends SimplePicture
     public void myCollage()
     {
         Picture ZiggyJulie = new Picture("ZiggyJulie.jpg");
-        Picture MB = new Picture("MusicBox.jpg");
-        Picture SSF = new Picture("SplitsStreetFair.jpg");
+        Picture ZSF = new Picture("SplitsStreetFair.jpg");
         this.copy(ZiggyJulie,0,0);
-        this.copy(SSF,100,0);
+        this.copy(ZSF,100,0);
         this.copy(ZiggyJulie,200,0);
-        Picture flowerNoBlue = new Picture(SSF);
+        Picture flowerNoBlue = new Picture(ZSF);
         flowerNoBlue.zeroBlue();
         this.copy(flowerNoBlue,300,0);
         this.copy(ZiggyJulie,400,0);
-        this.copy(SSF,500,0);
+        this.copy(ZSF,500,0);
         this.mirrorVertical();
-        Picture SSf1NoGreen = new Picture (SSF);
-        this.copy(MB, 35, 400);
         this.write("collage.jpg");
     }
 
@@ -270,123 +212,28 @@ public class Picture extends SimplePicture
      */
     public void edgeDetection(int edgeDist)
     {
-        //variables for left, right, top and bottom
-        Pixel leftPixel = null; 
+        Pixel leftPixel = null;
         Pixel rightPixel = null;
         Pixel topPixel = null;
         Pixel bottomPixel = null;
-        //variable for the pixels array
         Pixel[][] pixels = this.getPixels2D();
-        //variables for the right color and bottom color because you need to use them to get edges
         Color rightColor = null;
         Color bottomColor = null;
-        
-        //nested for loop to traverse pixels 2D array
         for (int row = 0; row < pixels.length; row++)
         {
             for (int col = 0; col < pixels[0].length-1; col++)
             {
-                //find each edge by setting left right top and bottom pixels to their row and column (where bottom is the next column over and right is the next column over
                 leftPixel = pixels[row][col];
                 rightPixel = pixels[row][col+1];
-                bottomPixel = pixels[row][col+1];
+                topPixel = pixels[row][col];
+                bottomPixel = pixels[row+1][col];
                 rightColor = rightPixel.getColor();
-                bottomColor = bottomPixel.getColor();
-                
-                //if statement to set the edge to black
-                    //if the right color has a large change from the left color or the top has a large change from the bottom, set it to black
-                if(leftPixel.colorDistance(rightColor) > edgeDist || leftPixel.colorDistance(bottomColor) > edgeDist){
-                    Color color = new Color (0, 225, 225);
-                    leftPixel.setColor(color);
-                    //leftPixel.setColor(Color.BLACK);
-                // if the change is not great, set the color to white
-                }else{
-                    leftPixel.setColor(Color.WHITE);
-                }
-            }
-
-        }
-    }
-    /* Method for edge detection Ziggy Style*/
-    public void myEdgeDetection(int edgeDist){
-        //variables to find each edge
-        //top, bottom, right, left
-        Pixel top = null;
-        Pixel bottom = null;
-        Pixel left = null; 
-        Pixel right = null;
-        Pixel[][] pixels = this.getPixels2D();
-        
-        Color leftColor = null;
-        Color rightColor = null;
-        Color topColor =  null;
-        Color bottomColor = null;
-        
-        //find difference in color between two pixels
-        //nested for loop to traverse pixels 2D array
-        for (int row = 0; row < pixels.length; row++)
-        {
-            for (int col = 0; col < pixels[0].length-1; col++)
-            {
-                //find each edge by setting left right top and bottom pixels to their row and column (where bottom is the next column over and right is the next column over
-                left = pixels[row][col];
-                right = pixels[row][col+1];
-                top=pixels[row][col];
-                bottom = pixels[row][col+1];
-                rightColor = right.getColor();
-                bottomColor = bottom.getColor();
-                //if statement to set the edge to black
-                    //if the right color has a large change from the left color or the top has a large change from the bottom, set it to black
-                if(left.colorDistance(rightColor) > edgeDist || top.colorDistance(bottomColor) > edgeDist){
-                    Color color = new Color (0, 225, 255);
-                    left.setColor(color);
-
-                // if the change is not great, set the color to white
-                }else{
-                    left.setColor(Color.BLACK);
-                    top.setColor(Color.BLACK);
-                }
-            }
-
-        }
-        //if difference is big, set top to white and bottom to black
-        //if difference is big, set left to white and righ to black
-        //set edges of picture to black
-    }
-    public void colorEdge(int edgeDist){
-        //variables for left, right, top and bottom
-        Pixel leftPixel = null; 
-        Pixel rightPixel = null;
-        //variable for the pixels array
-        Pixel[][] pixels = this.getPixels2D();
-        //variables for the right color and bottom color because you need to use them to get edges
-        Color rightColor = null;
-        Color bottomColor = null;
-        
-        //nested for loop to traverse pixels 2D array
-        for (int row = 0; row < pixels.length; row++)
-        {
-            for (int col = 0; col < pixels[0].length-1; col++)
-            {
-                //find each edge by setting left right top and bottom pixels to their row and column (where bottom is the next column over and right is the next column over
-                leftPixel = pixels[row][col];
-                rightPixel = pixels[row][col+1];
- 
-                rightColor = rightPixel.getColor();
-
-                //if statement to set the edge to black
-                    //if the right color has a large change from the left color or the top has a large change from the bottom, set it to black
-                if(leftPixel.colorDistance(rightColor) > edgeDist || leftPixel.colorDistance(bottomColor) > edgeDist){
-                   //variable to make colors
-                    Color color = new Color ((int)(Math.random()*100)+1, (int)(Math.random()*100)+1, (int)(Math.random()*100)+1);
-                    //make the edge in color
-                    leftPixel.setColor(color);
-                // if the change is not great, set the color to white
-                }else{
+                if (leftPixel.colorDistance(rightPixel.getColor()) > edgeDist ||
+                topPixel.colorDistance(bottomPixel.getColor()) > edgeDist)
                     leftPixel.setColor(Color.BLACK);
-                }
+                else
+                    leftPixel.setColor(Color.WHITE);
             }
-
         }
     }
 
@@ -395,10 +242,10 @@ public class Picture extends SimplePicture
      */
     public static void main(String[] args) 
     {
-        Picture ZiggyJulie = new Picture("ZiggyJulie.jpg");
-        ZiggyJulie.explore();
-        ZiggyJulie.zeroBlue();
-        ZiggyJulie.explore();
+        Picture beach = new Picture("beach.jpg");
+        beach.explore();
+        beach.zeroBlue();
+        beach.explore();
     }
 
 } // this } is the end of class Picture, put all new methods before this
--- classes/PictureTester.java
+++ classes/PictureTester.java
@@ -8,124 +8,84 @@
  */
 public class PictureTester
 {
-    /** Method to test zeroBlue */
-    public static void testZeroBlue()
-    {
-        Picture beach = new Picture("SplitsStreetFair.jpg");
-        beach.explore();
-        beach.zeroBlue();
-        beach.explore();
-    }
-
-    /** Method to test keepOnlyBlue */
-    public static void keepOnlyBlue()
-    {
-        Picture beach = new Picture("beach.jpg");
-        beach.explore();
-        beach.OnlyBlue();
-        beach.explore();
-    }
-
-    /** Method to test mirrorVertical */
-    public static void testMirrorVertical()
-    {
-        Picture caterpillar = new Picture("ZiggyJulie.jpg");
-        caterpillar.explore();
-        caterpillar.mirrorVertical();
-        caterpillar.explore();
-    }
-
-    /** Method to test mirrorVerticalRightToLeft */
-    public static void testMirrorVerticalRightToLeft()
-    {
-        Picture caterpillar = new Picture("caterpillar.jpg");
-        caterpillar.explore();
-        caterpillar.mirrorVerticalRightToLeft();
-        caterpillar.explore();
-    }
-    // /** Method to test mirrorHorizontal */
-    // public static void testHorizontal()
-    // {
-    // Picture caterpillar = new Picture("caterpillar.jpg");
-    // caterpillar.explore();
-    // caterpillar.mirrorHorizontal();
-    // caterpillar.explore();
-
-    /** Method to test mirrorTemple */
-    public static void testMirrorTemple()
-    {
-        Picture temple = new Picture("temple.jpg");
-        temple.explore();
-        temple.mirrorTemple();
-        temple.explore();
-    }
-
-    /** Method to test the collage method */
-    public static void testCollage()
-    {
-        Picture canvas = new Picture("640x480.jpg");
-        canvas.createCollage();
-        canvas.explore();
-    }
-
-    /** Method to test the collage method */
-    public static void myCollage()
-    {
-        Picture canvas = new Picture("480x600.jpg");
-        canvas.myCollage();
-        canvas.explore();
-    }
-
-    /** Method to test edgeDetection */
-    public static void testEdgeDetection()
-    {
-        Picture swan = new Picture("swan.jpg");
-        swan.edgeDetection(10);
-        swan.explore();
-    }
-
-    public static void testMyEdgeDetection()
-    {
-        Picture ZiggyJulie = new Picture("ZiggyJulie.jpg");
-        ZiggyJulie.myEdgeDetection(10);
-        ZiggyJulie.explore();
-    }
-
-    public static void colorEdge(){
-        Picture ZiggyJulie = new Picture("ZiggyJulie.jpg");
-        ZiggyJulie.colorEdge(9);
-        ZiggyJulie.explore();
-    }
-
-    /** Main method for testing.  Every class can have a main
-     * method in Java */
-    public static void main(String[] args)
-    {
-        // uncomment a call here to run a test
-        // and comment out the ones you don't want
-        // to run
-        //testZeroBlue();
-        //testKeepOnlyBlue();
-        //testKeepOnlyRed();
-        //testKeepOnlyGreen();
-        //testNegate();
-        //testGrayscale();
-        //testFixUnderwater();
-        //testMirrorVertical();
-        //testMirrorTemple();
-        //testMirrorArms();
-        //testMirrorGull();
-        //testMirrorDiagonal();
-        //testCollage();
-        //testCopy();
-        //testEdgeDetection();
-        //testEdgeDetection2();
-        //test
-        //testChromakey();
-        //testEncodeAndDecode();
-        //testGetCountRedOverValue(250);
-        //testSetRedToHalfValueInTopHalf();
-        //testClearBlueOverValue(200);
-        //testGetAverageForColumn(0);
-    }
+  /** Method to test zeroBlue */
+  public static void testZeroBlue()
+  {
+    Picture beach = new Picture("beach.jpg");
+    beach.explore();
+    beach.zeroBlue();
+    beach.explore();
+  }
+  
+  /** Method to test mirrorVertical */
+  public static void testMirrorVertical()
+  {
+    Picture caterpillar = new Picture("caterpillar.jpg");
+    caterpillar.explore();
+    caterpillar.mirrorVertical();
+    caterpillar.explore();
+  }
+  
+  /** Method to test mirrorTemple */
+  public static void testMirrorTemple()
+  {
+    Picture temple = new Picture("temple.jpg");
+    temple.explore();
+    temple.mirrorTemple();
+    temple.explore();
+  }
+  
+  /** Method to test the collage method */
+  public static void testCollage()
+  {
+    Picture canvas = new Picture("640x480.jpg");
+    canvas.createCollage();
+    canvas.explore();
+  }
+  
+  /** Method to test the collage method */
+  public static void myCollage()
+  {
+    Picture canvas = new Picture("640x480.jpg");
+    canvas.myCollage();
+    canvas.explore();
+  }
+  /** Method to test edgeDetection */
+  public static void testEdgeDetection()
+  {
+    Picture swan = new Picture("swan.jpg");
+    swan.edgeDetection(10);
+    swan.explore();
+  }
+  
+  /** Main method for testing.  Every class can have a main
+    * method in Java */
+  public static void main(String[] args)
+  {
+    // uncomment a call here to run a test
+    // and comment out the ones you don't want
+    // to run
+    testZeroBlue();
+    //testKeepOnlyBlue();
+    //testKeepOnlyRed();
+    //testKeepOnlyGreen();
+    //testNegate();
+    //testGrayscale();
+    //testFixUnderwater();
+    //testMirrorVertical();
+    //testMirrorTemple();
+    //testMirrorArms();
+    //testMirrorGull();
+    //testMirrorDiagonal();
+    //testCollage();
+    //testCopy();
+    //testEdgeDetection();
+    //testEdgeDetection2();
+    //testChromakey();
+    //testEncodeAndDecode();
+    //testGetCountRedOverValue(250);
+    //testSetRedToHalfValueInTopHalf();
+    //testClearBlueOverValue(200);
+    //testGetAverageForColumn(0);
+  }
 }
