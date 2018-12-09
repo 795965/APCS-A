2a8b34295013ead52801367ba0dbe5a432b47558
795965 <42558488+795965@users.noreply.github.com>
2018-12-09 11:43:18 -0800

    Project Prime Numbers - no addNumbers

--- Lab 1130 Prime Numbers/PrimeNumbers.java
+++ Lab 1130 Prime Numbers/PrimeNumbers.java
@@ -8,12 +8,32 @@
 import java.util.ArrayList;
 public class PrimeNumbers
 {
+    //Initialize arraylist
+    private ArrayList<Integer>numbers;
+    
+    // //contructor
+    // public PrimeNumber(){
+        // numbers = new ArrayList<Integer>();
+    // }
+    
     //takes an integer as an input and puts each of the digits into 
     //an ArrayList of Integer objects.  
     public  void setListToNumbers (int n){
+        while(n>0){
+            numbers.add(0, n % 10);
+            n = n / 10;
+        }
+        //printList();
     }
     //takes an integer and adds it to an existing ArrayList of Integer objects
     public void addNumberToList(int n){
+        for(int i = 0; i < numbers.size(); i++){
+            if(n>0){
+                int swtich = numbers.size() -1 -i;
+                numbers.set(swtich, numbers.get(swtich) + n % 10);
+                n = n /10;
+            }
+        }
     }
     //takes an integer and adds it to an existing ArrayList of Integer 
     //objects
