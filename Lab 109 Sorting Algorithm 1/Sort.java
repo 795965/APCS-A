8aefc9bac310644e1553af8a34d3e97a86140549
795965 <42558488+795965@users.noreply.github.com>
2019-01-09 23:35:24 -0800

    I don't know

---
+++ Lab 109 Sorting Algorithm 1/Sort.java
@@ -0,0 +1,29 @@
+
+/**
+ * From our psuedocode, designed to oragnize array into new array, smallest to largest
+ *
+ * @author (Ziggy Sheynin)
+ * @version (aLab 109)
+ * assume all numbers are positive
+ */
+import java.util.*;
+public class Sort
+{
+    public static void main(){
+
+        //declare and initialze array
+        int [] unordered = new int [10];
+        for (int i = 0; i < unordered.length; i++){
+            int x = (int)(Math.random()*10)+1;
+            int [i] = new int(x);
+        }
+
+    }
+    //declare and initialize varible small, set = 0
+    //traverse array
+
+    //find all values = to small
+    //put values into new array
+    //if no value = small, add one to small and repeat
+
+}
